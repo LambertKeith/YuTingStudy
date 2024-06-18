c893001aabd0019bfb84881f9686c282187d421c
keith <1>
2024-06-18 16:33:42 +0800

    1

---
+++ dev_run.py
@@ -0,0 +1,7 @@
+from .servers.insert_server import TableInserter
+
+
+
+if __name__ == "__main__":
+    table_inserter = TableInserter()
+    
---
+++ servers/insert_server.py
@@ -0,0 +1,18 @@
+
+
+
+
+class TableInserter:
+    # TODO
+    # 所有的操作都在这里执行
+    def __init__(self) -> None:
+
+        pass
+    
+    
+    def func1(args):
+        pass
+
+
+    def func2(args):
+        pass
